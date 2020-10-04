62cca0379932e16bbb9b550b3e179bf31fc698b8
JOSE DAVID PINTO LUCAS <jodapilu01@gmail.com>
2020-10-04 18:55:41 -0500

    proyecto galeria

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-
 var btn = document.getElementsByClassName("btn");
- var fotos = document.getElementById("fotos");
+var fotos = document.getElementById("fotos");
 
- var imagenes = new Array(
-     "imagenes/pic1.jpg", 
-     "imagenes/pic2.jpg", 
-     "imagenes/pic3.jpg", 
-     "imagenes/pic4.jpg", 
- );
+var imagenes = new Array(
+    "imagenes/pic1.jpg",
+    "imagenes/pic2.jpg",
+    "imagenes/pic3.jpg",
+    "imagenes/pic4.jpg",
+);
 
- for(lef i=0; i<btn.length; i++){
-     btn [i].onclick =function(){
-         fotos.src=imagenes[i];
-     };
- }
+for (Left i = 0; i < btn.length; i++) {
+    btn[i].onclick = function () {
+        fotos.src = imagenes[i];
+         current = document.getElementsByClassName("activar");
+        current[0].ClassName = current[0].ClassName.replace("activar", "");
+        this.className += "activar";
+    };
+}
